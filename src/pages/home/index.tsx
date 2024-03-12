6eaa8acf51682604b3a92c2f56e724bd3ba596b7
Jorge <jorgelucastcoronado@gmail.com>
2024-03-12 18:51:32 -0300

    fix: resolving build problems

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -13,6 +13,7 @@ import { FeedItem } from "../../components/FeedItem";
 const { height: heightScreen } = Dimensions.get("screen");
 
 export default function Home() {
+  console.log("home")
   const [showItem, setShowItem] = useState(mockedPosts[0]);
   const onViewRef = useRef(({ viewableItems }: any) => {
     if (viewableItems && viewableItems?.length > 0) {
